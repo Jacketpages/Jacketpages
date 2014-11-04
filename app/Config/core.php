d4171a814db8ec10c323b33e05394cb5f0eaa263
Michael Ozeryansky <mozer624@gmail.com>
2014-11-03 23:34:24 -0500

    Remote server's engine must be Memcache

--- app/Config/core.php
+++ app/Config/core.php
@@ -319,7 +319,7 @@
  *       Please check the comments in boostrap.php for more info on the cache engines available
  *       and their settings.
  */
-$engine = 'File';
+$engine = 'Memcache';
 
 // In development mode, caches should expire quickly.
 $duration = '+999 days';
